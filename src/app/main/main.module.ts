e7910a442a3658c3f1b85e8c4a374aaf9bb7bae4
elkhailihamza <elkhailihamza331@gmail.com>
2025-02-27 12:12:17 +0000

    designing page

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { MainRoutingModule } from './main-routing.module';
 import { LayoutModule } from './layout/layout.module';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    ContentComponent
   ],
   imports: [
     CommonModule,
